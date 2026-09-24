f520b9eb16610d7187a3bf71607a52bdb61d7d98
Jessica <susanjessica.v@gmail.com>
2026-09-24 22:59:32 +1000

    Update header text to demonstrate continuous deployment

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -43,7 +43,7 @@ const Header = () => {
             fontWeight: 600,
           }}
         >
-          KoalaTech University
+          KoalaTech University — Live Deployment
         </Typography>
 
         <Box
